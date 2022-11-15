169cc2e6297e784339144ca992e2d3015e88ff1b
CollidaCube <68357485+CollidaCube@users.noreply.github.com>
2022-11-14 17:54:48 -0700

    Create Tools.js

---
+++ Tools.js
@@ -0,0 +1,13 @@
+// These are less powerful utilities to facilitate blooket for you.
+// You can enable and disable features by editing the properties below
+// Once you have edited the properties, copy and paste the code in the 'console' section of the Chrome Dev Tools (ctrl+shift+I)
+
+const properties = {
+  'AutoAnswer': true, // Whenever you open the quiz menu, it will automatically answer it for you.
+  'FeedAll': true, // [Cafe] When you select a food, it will be automatically distributed to all blooks.
+  'ServeAll': true, // As soon as you complete a quiz, food will be automatically distributed to all blooks.
+}
+
+// DO NOT TOUCH - Any code changed after this point is at your own risk
+
+// ...
